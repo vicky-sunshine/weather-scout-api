d9d00a85348a399724cd8fb85ec810534d50a402
vicky <lch82327@gmail.com>
2016-01-02 17:23:36 +0800

    add negative test

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -19,4 +19,22 @@ describe 'Look up the weather today' do
     post '/api/v1/daily_weather', body.to_json, header
     last_response.must_be :ok?
   end
+
+  it 'Should return 404 for unknown city (not major city)' do
+    header = { 'CONTENT_TYPE' => 'application/json' }
+    body = {
+      city: '台東市'
+    }
+
+    post '/api/v1/daily_weather', body.to_json, header
+    last_response.must_be :not_found?
+  end
+
+  it 'Should return 400 for bad json format' do
+    header = { 'CONTENT_TYPE' => 'application/json' }
+    body = "blah blah"
+
+    post '/api/v1/daily_weather', body.to_json, header
+    last_response.must_be :bad_request?
+  end
 end
