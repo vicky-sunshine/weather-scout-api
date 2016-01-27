8f822bafbdb0f6d6e3e762c4ade1357785bbe876
vicky <lch82327@gmail.com>
2016-01-28 00:10:18 +0800

    add testcase for weekly weather

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -38,3 +38,33 @@ describe 'Look up the weather today' do
     last_response.must_be :bad_request?
   end
 end
+
+describe 'Look up the weather this week' do
+  it 'Should return ok' do
+    header = { 'CONTENT_TYPE' => 'application/json' }
+    body = {
+      city: '新竹'
+    }
+
+    post '/api/v1/weekly_weather', body.to_json, header
+    last_response.must_be :ok?
+  end
+
+  it 'Should return 404 for unknown city (not major city)' do
+    header = { 'CONTENT_TYPE' => 'application/json' }
+    body = {
+      city: '台東市'
+    }
+
+    post '/api/v1/weekly_weather', body.to_json, header
+    last_response.must_be :not_found?
+  end
+
+  it 'Should return 400 for bad json format' do
+    header = { 'CONTENT_TYPE' => 'application/json' }
+    body = "blah blah"
+
+    post '/api/v1/weekly_weather', body.to_json, header
+    last_response.must_be :bad_request?
+  end
+end
